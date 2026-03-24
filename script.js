8bda72ab212a9698554b978db28750c6c65a9f73
cookie560 <11230247@dcsh.tp.edu.tw>
2026-03-24 14:49:33 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showSection(id) {
-    let sections = document.querySelectorAll("section");
-    sections.forEach(sec => sec.classList.remove("active"));
-
-    document.getElementById(id).classList.add("active");
-}
